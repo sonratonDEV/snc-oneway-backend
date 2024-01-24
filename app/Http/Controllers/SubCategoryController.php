c330553d140af1977664937edd0c27b3e7be4f33
sonratonDEV <sonraton.n@gmail.com>
2024-01-24 16:44:42 +0700

    Finish role access sub function and cachekey

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator; //ตัวเช็คข้อมูล
 use Illuminate\Support\Facades\DB; //import database
-
+use Illuminate\Support\Facades\Cache;
 use App\Http\Libraries\JWT\JWTUtils; //JWT
 
 use DateTime;
@@ -30,6 +30,7 @@ class SubCategoryController extends Controller
                     "data" => [],
                 ], 401);
             }
+            $decoded = $jwt->decoded;
 
             $rules = [
                 "main_category_id"      => ["required", "uuid"],
@@ -48,16 +49,59 @@ class SubCategoryController extends Controller
                 ],400);
             }
 
-            DB::table('tb_sub_service_categories')->insert([
-                "main_category_id" => $request->main_category_id,
-                'sub_category_desc' => $request->sub_category_desc
-            ]);
+            //decode role from token
+            $roleToken = $decoded->role;
+            $role_id = DB::table('tb_role as t1')->selectRaw('*')->leftJoin('tb_role_function as t2','t2.role_id','=','t1.role_id')->get();
 
+            foreach ($role_id as $doc) {
+                $role = [
+                    'role_id' => $doc->role_id,
+                    'role_desc' => $doc->role_desc,
+                    'data' => [
+                        'created_at' => $doc->created_at,
+                        'updated_at' => $doc->updated_at,
+                        'role_function_id' => $doc->role_function_id,
+                        'function_desc' => $doc->function_desc,
+                        'is_available' => $doc->is_available,
+                    ],
+                ];
+            
+                // Debugging: Print information for each iteration
+                echo "Role: {$doc->role_desc}, Function: {$doc->function_desc}, isAvailable: {$doc->is_available}\n";
+            
+                // Check for "Create main categories" function availability for any role
+                if ($roleToken == $doc->role_desc && $doc->function_desc == 'Create sub categories' && $doc->is_available == true) {
+                    
+                    $categoryCheck = DB::table('tb_sub_service_categories')->select('*')->where('sub_category_desc',$request->sub_category_desc)->get();
+
+                    if(count($categoryCheck) !=0){
+                        return response()->json([
+                            "status" => "error",
+                            "message" => "the sub category has been in system",
+                            "data" =>[]
+
+                        ]);
+                    }
+            
+                    // Insert data into the database
+                    DB::table('tb_sub_service_categories')->insert([
+                        "main_category_id" => $request->main_category_id,
+                        'sub_category_desc' => $request->sub_category_desc
+                    ]);
+
+                    return response()->json([
+                        "status"    => "success",
+                        "message"   => 'Insert data successfully',
+                        "data"      => [$request->sub_category_desc],
+                    ], 200);
+                }
+            }
+            // If the loop completes without finding a match, return an error
             return response()->json([
-                "status"    => "success",
-                "message"   => 'Insert data successfully',
-                "data"      => [$request->sub_category_desc],
-            ], 200);
+                "status" => "error",
+                "message" => "Cannot access, you don't have permission.",
+                "data" => [],
+            ]);
 
         } catch (\Exception $e) {
             return response()->json([
@@ -80,8 +124,18 @@ class SubCategoryController extends Controller
                     "data" => [],
                 ]);
             }
+            // API Caching
+            $cacheKey = "/sub-oneway/get-all-cache";
+            $cacheData = Cache::get($cacheKey);
+            if (!is_null($cacheData))  return response([
+                "status" => "success",
+                "message"=> "Data from chaced",
+                "data" => json_decode($cacheData)
+            ]);
 
             $get = DB::table('tb_sub_service_categories')->select('*')->get();
+
+            Cache::put($cacheKey, \json_encode($get, JSON_UNESCAPED_UNICODE), \DateInterval::createFromDateString('1 minutes'));
 
             return response()->json([
                 "status"    => "success",
@@ -109,6 +163,9 @@ class SubCategoryController extends Controller
                 "message" => "Unauthorized",
                 "data" => []
             ], 401);
+            $decoded = $jwt->decoded;
+            //decode role from token
+            $roleToken = $decoded->role;
 
             $rules = [
                 "sub_category_id"      => ["required", "uuid"],
@@ -125,19 +182,56 @@ class SubCategoryController extends Controller
                 ]
             ], 400);
 
-            DB::table("tb_sub_service_categories")->where(
-                "sub_category_id", $request->sub_category_id)->where(
-                "main_category_id", $request->main_category_id
-            )->update([
-                "sub_category_desc"    => $request->sub_category_desc,
-                "updated_at"            => DB::raw("now()"),
+            $role_id = DB::table('tb_role as t1')->selectRaw('*')->leftJoin('tb_role_function as t2','t2.role_id','=','t1.role_id')->get();
+
+            foreach ($role_id as $doc) {
+                $role = [
+                    'role_id' => $doc->role_id,
+                    'role_desc' => $doc->role_desc,
+                    'data' => [
+                        'created_at' => $doc->created_at,
+                        'updated_at' => $doc->updated_at,
+                        'role_function_id' => $doc->role_function_id,
+                        'function_desc' => $doc->function_desc,
+                        'is_available' => $doc->is_available,
+                    ],
+                ];
+                // Debugging: Print information for each iteration
+                echo "Role: {$doc->role_desc}, Function: {$doc->function_desc}, isAvailable: {$doc->is_available}\n";
+
+                // Check for "Create main categories" function availability for any role
+                if ($roleToken == $doc->role_desc && $doc->function_desc == 'Update sub categories' && $doc->is_available == true) {
+                    $categoryCheck = DB::table('tb_sub_service_categories')->select('*')->where('sub_category_desc',$request->sub_category_desc)->get();
+
+                    if(count($categoryCheck) !=0){
+                        return response()->json([
+                            "status" => "error",
+                            "message" => "the sub category has been in system",
+                            "data" =>[]
+                        ]);
+                    }
+
+                DB::table("tb_sub_service_categories")->where(
+                    "sub_category_id", $request->sub_category_id)->where(
+                    "main_category_id", $request->main_category_id
+                )->update([
+                    "sub_category_desc"    => $request->sub_category_desc,
+                    "updated_at"            => DB::raw("now()"),
+                ]);
+
+                return response()->json([
+                    "status" => "success",
+                    "message" => "Updated sub category success",
+                    "data" => [$request->sub_category_desc],
+                ], 201);
+            }}
+            // If the loop completes without finding a match, return an error
+            return response()->json([
+                "status" => "error",
+                "message" => "Cannot access, you don't have permission.",
+                "data" => [],
             ]);
 
-            return response()->json([
-                "status" => "success",
-                "message" => "Updated sub category success",
-                "data" => [$request->sub_category_desc],
-            ], 201);
         } catch (\Exception $e) {
             return response()->json([
                 "status"    => "error",
@@ -158,6 +252,9 @@ class SubCategoryController extends Controller
                 "message" => "Unauthorized",
                 "data" => []
             ], 401);
+            $decoded = $jwt->decoded;
+            //decode role from token
+            $roleToken = $decoded->role;
 
             $rules = [
                 "sub_category_id"      => ["required", "uuid"],
@@ -173,33 +270,54 @@ class SubCategoryController extends Controller
                 ]
             ], 400);
 
-            // DB::table("tb_sub_service_categories")->where("sub_category_id", $request->sub_category_id)->where("main_category_id", $request->main_category_id)->delete();
-            $count_result= DB::table("tb_services")->where("sub_category_id", $request->sub_category_id)->where("main_category_id", $request->main_category_id)->count();
-            
-            if(($count_result) !=0 ){
-                return response()->json([
-                    "status" => "success",
-                    "message" => "Can not deleted main category",
-                    "data" => [
-                            $count_result
+            $role_id = DB::table('tb_role as t1')->selectRaw('*')->leftJoin('tb_role_function as t2','t2.role_id','=','t1.role_id')->get();
+
+            foreach ($role_id as $doc) {
+                $role = [
+                    'role_id' => $doc->role_id,
+                    'role_desc' => $doc->role_desc,
+                    'data' => [
+                        'created_at' => $doc->created_at,
+                        'updated_at' => $doc->updated_at,
+                        'role_function_id' => $doc->role_function_id,
+                        'function_desc' => $doc->function_desc,
+                        'is_available' => $doc->is_available,
                     ],
+                ];
+
+                // Debugging: Print information for each iteration
+                echo "Role: {$doc->role_desc}, Function: {$doc->function_desc}, isAvailable: {$doc->is_available}\n";
+
+                // Check for "Create main categories" function availability for any role
+                if ($roleToken == $doc->role_desc && $doc->function_desc == 'Delete sub categories' && $doc->is_available == true) {
+
+                $count_result= DB::table("tb_services")->where("sub_category_id", $request->sub_category_id)->where("main_category_id", $request->main_category_id)->count();
+            
+                if(($count_result) !=0 ){
+                    return response()->json([
+                        "status" => "success",
+                        "message" => "Can not deleted main category",
+                        "data" => [
+                                $count_result
+                        ],
+                    ], 201);
+
+                } else{
+                    DB::table("tb_sub_service_categories")->where("sub_category_id", $request->sub_category_id)->where("main_category_id", $request->main_category_id)->delete();
+
+                    return response()->json([
+                    "status" => "success",
+                    "message" => "Deleted sub category success",
+                    "data" => [],
                 ], 201);
+                }}}
 
-            } else{
-                DB::table("tb_sub_service_categories")->where("sub_category_id", $request->sub_category_id)->where("main_category_id", $request->main_category_id)->delete();
-
-                return response()->json([
-                "status" => "success",
-                "message" => "Deleted sub category success",
+            // If the loop completes without finding a match, return an error
+            return response()->json([
+                "status" => "error",
+                "message" => "Cannot access, you don't have permission.",
                 "data" => [],
-            ], 201);
-            }
-
-            // return response()->json([
-            //     "status" => "success",
-            //     "message" => "Deleted main category success",
-            //     "data" => [],
-            // ], 201);
+            ]);
 
         } catch (\Exception $e) {
             return response()->json([
